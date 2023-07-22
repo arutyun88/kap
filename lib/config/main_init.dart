fddafc9a9cd6bef000070099ade6e3256f09efda
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2023-07-23 00:53:06 +0400

    firebase init

--- lib/config/main_init.dart
+++ lib/config/main_init.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kap/app/modules/my_app.dart';
 import 'package:kap/config/environment.dart';
+import 'package:kap/firebase_options.dart';
 import 'package:kap/services/environment_service.dart';
 
 void mainInit(Environment env) async {
@@ -10,4 +12,5 @@ void mainInit(Environment env) async {
 
 Future<void> _initServices({required Environment env}) async {
   EnvironmentService.init(env);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
