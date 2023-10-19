d7fe560b17c5561ee426aa4961f22bede504c61a
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2023-10-19 22:51:22 +0300

    service: environment service

--- lib/services/environment_service.dart
+++ lib/services/environment_service.dart
@@ -1,18 +1,23 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+import 'package:path_provider/path_provider.dart';
 
 import '../config/environment.dart';
 
 class EnvironmentService extends GetxService {
-  EnvironmentService._(this.environment, this.appInfo);
+  EnvironmentService._(this.environment, this.appInfo, this.documentsDirectory);
 
   static Future<void> init(Environment env) async {
     final info = await PackageInfo.fromPlatform();
-    Get.lazyPut(() => EnvironmentService._(env, info));
+    final documentsDirectory = await getApplicationDocumentsDirectory();
+    Get.lazyPut(() => EnvironmentService._(env, info, documentsDirectory));
   }
 
   final Environment environment;
   final PackageInfo appInfo;
+  final Directory documentsDirectory;
 
   static EnvironmentService to = Get.find<EnvironmentService>();
 }
