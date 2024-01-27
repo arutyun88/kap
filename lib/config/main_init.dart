2b636daf03e632d25e1934426056f62eec8e1ad8
Arutyun Gevorkyan <gevorkyan.arutyun@gmail.com>
2024-01-27 16:42:41 +0300

    feature: init: services

--- lib/config/main_init.dart
+++ lib/config/main_init.dart
@@ -1,11 +1,18 @@
 import 'dart:developer';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_libphonenumber/flutter_libphonenumber.dart' as phone;
 import 'package:kap/app/modules/my_app.dart';
 import 'package:kap/config/environment.dart';
+import 'package:kap/datasource/authorization/firebase_authorization_datasource.dart';
+import 'package:kap/datasource/authorization/hive_authorization_datasource.dart';
+import 'package:kap/datasource/device/firebase_device_datasource.dart';
 import 'package:kap/datasource/localization/firebase_localization_datasource.dart';
 import 'package:kap/datasource/localization/hive_localization_datasource.dart';
+import 'package:kap/repositories/authorization_repository.dart';
 import 'package:kap/repositories/localization_repository.dart';
 import 'package:kap/services/environment_service.dart';
 import 'package:kap/services/firebase_service.dart';
@@ -31,18 +38,25 @@ Future<void> _initServices({required Environment env}) async {
   LaunchTrackerService.to.startService('StorageService');
   await StorageService.init();
   LaunchTrackerService.to.stopService('StorageService');
-  LaunchTrackerService.to.startService('LoggingService');
-  await AuthService.init();
-  LaunchTrackerService.to.stopService('LoggingService');
+  LaunchTrackerService.to.startService('FirebaseService');
+  await FirebaseService.init();
+  LaunchTrackerService.to.stopService('FirebaseService');
+  LaunchTrackerService.to.startService('AuthService');
+  await phone.init();
+  await AuthService.init(
+    AuthorizationRepository(
+      remoteAuthorizationDatasource: FirebaseAuthorizationDatasource.init(FirebaseAuth.instance),
+      localAuthorizationDatasource: HiveAuthorizationDatasource(),
+      deviceDatasource: FirebaseDeviceDatasource.init(FirebaseFirestore.instance),
+    ),
+  );
+  LaunchTrackerService.to.stopService('AuthService');
   LaunchTrackerService.to.startService('ThemeService');
   await ThemeService.init();
   LaunchTrackerService.to.stopService('ThemeService');
   LaunchTrackerService.to.startService('ObserverService');
   ObserverService.init();
   LaunchTrackerService.to.stopService('ObserverService');
-  LaunchTrackerService.to.startService('FirebaseService');
-  await FirebaseService.init();
-  LaunchTrackerService.to.stopService('FirebaseService');
   LaunchTrackerService.to.startService('LocalizationService');
   await LocalizationService.init(
     LocalizationRepository(
